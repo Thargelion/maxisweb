9816cfa012028afe1922327c3f7b443d7856755a
Thargelion <elwindwalker@gmail.com>
2014-12-09 15:55:21 -0300

    modified:   mensajes.php

--- mensajes.php
+++ mensajes.php
@@ -5,6 +5,8 @@
 	<?php header('Content-Type: text/html; charset=utf-8'); ?>
 	<link href="css/estilo3.css" rel="stylesheet">
 	<link href="css/trones2.css" rel="stylesheet">
+	<link href="css/estilo3.css" rel="stylesheet">
+	<link href="css/trones2.css" rel="stylesheet">
 </head>
 <html>
 	<body>
@@ -12,10 +14,13 @@
 		$name = $_POST['nombre'];
 		$mail = $_POST['email'];
 		$comentario = $_POST['comentario'];
-		
+		?>
 		echo $name;
 		echo $mail;
 		echo $comentario;
-		?>
+		<header id="elmaxi">
+			<img id="maxbol" src="images/maxi1.gif" alt="Maxi Árbol">
+			<div></div>
+		</header>
 	</body>
 </html>
