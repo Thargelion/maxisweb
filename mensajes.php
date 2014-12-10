b32c347730191e747fd9c62acb583a9514ba70be
Thargelion <elwindwalker@gmail.com>
2014-12-10 18:12:29 -0300

    modified:   css/estilo3.css 	modified:   index2.html 	modified:   mensajes.php

--- mensajes.php
+++ mensajes.php
@@ -15,15 +15,24 @@
 		$mail = $_POST['email'];
 		$comentario = $_POST['comentario'];
 		?>
-		echo $name;
-		echo $mail;
-		echo $comentario;
 		<header id="elmaxi">
 			<img id="maxbol" src="images/maxi1.gif" alt="Maxi Árbol">
-			<div class="hoja" onclick="goBack()">
+			<div class="hoja" onclick="goBack()" style="float left";>
 				Volver Atrás
 			</div>
 		</header>
+		<article class="maxitron" id="comentarios">
+			<section class="comentario maxitron">
+					<div id="nombre"><h2>Nombre</h2>
+						<?php echo $name . "<br>"; ?>
+					</div>
+					<div id="mail"><h2>Mail</h2>
+						<?php echo $mail . "<br>"; ?>
+					</div>
+					<h2>Comentario</h2>
+						<?php echo $comentario ."<br>"; ?>
+			</section>
+		</article>
 
 		<script src="js/jquery-1.11.1.min.js"></script>
 		<script src="js/index.js"></script>
