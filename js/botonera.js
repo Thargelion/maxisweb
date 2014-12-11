2a4c34458bd3f0666a0159957381819ae85cf0a0
Thargelion <elwindwalker@gmail.com>
2014-12-10 21:40:41 -0300

    new file:   botonera.html 	new file:   css/botonera.css 	new file:   js/botonera.js

---
+++ js/botonera.js
@@ -0,0 +1,150 @@
+// botonera.js
+
+
+// your monety
+$('#b1s').trigger('load');
+
+$('#b1').click(function(){
+	$('#b1s').trigger('play');
+});
+
+//I see no
+$('#b2s').trigger('load');
+
+$('#b2').click(function(){
+	$('#b2s').trigger('play');
+});
+
+//I will have our
+$('#b3s').trigger('load');
+
+$('#b3').click(function(){
+	$('#b3s').trigger('play');
+});
+
+//Stand and deliver
+$('#b4s').trigger('load');
+
+$('#b4').click(function(){
+	$('#b4s').trigger('play');
+});
+
+//My men...
+$('#b5s').trigger('load');
+
+$('#b5').click(function(){
+	$('#b5s').trigger('play');
+});
+
+//We can do this
+$('#b6s').trigger('load');
+
+$('#b6').click(function(){
+	$('#b6s').trigger('play');
+});
+
+//Everything has a prize...
+$('#b7s').trigger('load');
+
+$('#b7').click(function(){
+	$('#b7s').trigger('play');
+});
+
+//It's almost...
+$('#f1s').trigger('load');
+
+$('#f1').click(function(){
+	$('#f1s').trigger('play');
+});
+
+//Ah!
+$('#f2s').trigger('load');
+
+$('#f2').click(function(){
+	$('#f2s').trigger('play');
+});
+
+//My lord!
+$('#f3s').trigger('load');
+
+$('#f3').click(function(){
+	$('#f3s').trigger('play');
+});
+
+//My lord! II
+$('#f4s').trigger('load');
+
+$('#f4').click(function(){
+	$('#f4s').trigger('play');
+});
+
+//Away with you!
+$('#n1s').trigger('load');
+
+$('#n1').click(function(){
+	$('#n1s').trigger('play');
+});
+
+//You better
+$('#r1s').trigger('load');
+
+$('#r1').click(function(){
+	$('#r1s').trigger('play');
+});
+
+//What is this
+$('#r2s').trigger('load');
+
+$('#r2').click(function(){
+	$('#r2s').trigger('play');
+});
+
+//Our stroll
+$('#r3s').trigger('load');
+
+$('#r3').click(function(){
+	$('#r3s').trigger('play');
+});
+
+//We're gonna
+$('#r4s').trigger('load');
+
+$('#r4').click(function(){
+	$('#r4s').trigger('play');
+});
+
+
+//I dare you
+$('#r5s').trigger('load');
+
+$('#r5').click(function(){
+	$('#r5s').trigger('play');
+});
+
+//I will drink
+$('#s1s').trigger('load');
+
+$('#s1').click(function(){
+	$('#s1s').trigger('play');
+});
+
+//Today gods..
+$('#s2s').trigger('load');
+
+$('#s2').click(function(){
+	$('#s2s').trigger('play');
+});
+
+//That's a nice...
+$('#s3s').trigger('load');
+
+$('#s3').click(function(){
+	$('#s3s').trigger('play');
+});
+
+//Less talking
+$('#s4s').trigger('load');
+
+$('#s4').click(function(){
+	$('#s4s').trigger('play');
+});
